ed347bb6054a5ff5e6baa1c193424a9074f76d9d
wrobellkatarzyna <63967309+wrobellkatarzyna@users.noreply.github.com>
2020-09-21 12:55:25 +0200

    add table

--- app_12_GeoCoder.py
+++ app_12_GeoCoder.py
@@ -1,35 +1,42 @@
+from io import StringIO
+
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import pandas as pd
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-def file_to_string(filepath):
-    text = textract.process(filepath)
-    print (text)
-    return text
 
 @app.route("/success", methods=['POST'])
 def success():
     global file
     if request.method == 'POST':
         file = request.files["file"]
-        data = file.read()
-        #file.save(secure_filename("uploaded " + file.filename))
-        #request.session['text'] = file_to_string(file.file.path)
-        #data = pd.read_csv(file.name)
-        #print(request.session['text'])
+        data = StringIO(file.read().decode("utf-8"))
+        global df
+        df = pd.read_csv(data, sep=',', engine='python')
 
-        return render_template("index.html", btn="download.html")
+        if "Address" and "address" not in df.columns:
+            return render_template("index.html",text="Your file does not contain Address column.")
+
+        df["Longitude"] = "lon"
+        df["latitude"] = "lat"
+
+        return render_template("index.html", btn="download.html", tables=[df.to_html(classes='data', header="true", index=False)])
+
 
 @app.route("/download")
 def download():
-    pass
+    df.to_csv(
+        r'C:\Users\Katrina\PycharmProjects\HomeTraining\app_12_GeoCoder\Your csv file.csv',index = False)
+    #file.save(secure_filename("uploaded " + file.filename))
+    return render_template("success.html")
+
 
 if __name__ == '__main__':
     app.debug = True

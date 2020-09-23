947a0ee35e142cb8df7995a79186d9ccb405709d
wrobellkatarzyna <63967309+wrobellkatarzyna@users.noreply.github.com>
2020-09-23 10:20:31 +0200

    final version

--- app_12_GeoCoder.py
+++ app_12_GeoCoder.py
@@ -1,8 +1,8 @@
-from io import StringIO
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 import pandas as pd
+from io import StringIO
+import datetime
 
 app = Flask(__name__)
 
@@ -14,28 +14,34 @@ def index():
 
 @app.route("/success", methods=['POST'])
 def success():
-    global file
+    global filename
     if request.method == 'POST':
         file = request.files["file"]
+
         data = StringIO(file.read().decode("utf-8"))
-        global df
         df = pd.read_csv(data, sep=',', engine='python')
 
         if "Address" and "address" not in df.columns:
-            return render_template("index.html",text="Your file does not contain Address column.")
+            return render_template("index.html",
+                                   text="Your file does not contain Address column.")
 
         df["Longitude"] = "lon"
         df["latitude"] = "lat"
 
-        return render_template("index.html", btn="download.html", tables=[df.to_html(classes='data', header="true", index=False)])
+        filename = "Your csv file " + datetime.datetime.now().strftime(
+            '%Y%m%d-%H%M%S') + ".csv"
+        import os
+        filename =os.path.join("uploads",filename)
+        df.to_csv(filename, index=False)
+
+        return render_template("index.html", btn="download.html", tables=[
+            df.to_html(classes='data', header="true", index=False)])
 
 
 @app.route("/download")
 def download():
-    df.to_csv(
-        r'C:\Users\Katrina\PycharmProjects\HomeTraining\app_12_GeoCoder\Your csv file.csv',index = False)
-    #file.save(secure_filename("uploaded " + file.filename))
-    return render_template("success.html")
+    return send_file(filename,attachment_filename='yourfile.csv', as_attachment=True)
+    #return render_template("success.html")
 
 
 if __name__ == '__main__':
